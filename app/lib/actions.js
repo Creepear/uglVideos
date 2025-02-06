a10013c44dd15b3e1301e8f738c5911d1bf6f350
Creepear <mcreepear@gmail.com>
2025-02-06 19:24:45 +0100

    Update actions.js

--- app/lib/actions.js
+++ app/lib/actions.js
@@ -3,6 +3,6 @@ export async function fetchVideo() {
     if(token.seenVideo === true) {
         return {message:"Sorry, hast das Video schon gesehen, frag Basti wenn du es nochmal schauen willst.", url:false}
     }else {
-        return {message: "The Cake is a Lie - Portal" ,url:"https://youtu.be/-g598UJ7rX0"}
+        return {message: "The Cake is a Lie - Portal" ,url:"https://drive.google.com/file/d/1YEWmUFfV5tBt5eX-dfou8beB5EGM4NPB/view?usp=drive_link"}
     }
 }
